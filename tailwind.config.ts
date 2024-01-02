41c79e148cd9fd96b2bf7e40f1ab8081015740ad
TylerIllman <tyler.illman.productions@gmail.com>
2024-01-02 08:35:54 +0800

    Landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
-const config: Config = {
-  darkMode: "class",
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
     container: {
       center: true,
       padding: "2rem",
-      screens: {"2xl": "1400px"}
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      maxWidth: {"8xl": "1408px"},
       colors: {
-        border:"hsl(var(--border))",
-        input:"hsl(var(--input))",
-        ring:"hsl(var(--ring))",
-        background:"hsl(var(--background))",
-        foreground:"hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -52,17 +55,17 @@ const config: Config = {
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "calc(var(--radius) -2px)",
-        sm: "calc(var(--radius) -4px)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
-          from: {height:"0"},
-          to: {height: "var(--radix-accordion-content-height"},
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
-          from: {height: "var(--radix-accordion-content-height"},
-          to:{height:"0"},
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -71,6 +74,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-}
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
 export default config
