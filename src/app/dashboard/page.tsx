2d3ca9393d033fd49f3739ecf172f6947b7cec0f
TylerIllman <tyler.illman.productions@gmail.com>
2024-01-05 18:31:00 +0800

    Dashboard page user logged in and in DB checks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div>Dashboard</div>;
-};
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "~/server/db";
 
-export default Page;
+export default async function Page() {
+  const user = await currentUser();
+
+  if (!user || !user.id) redirect("/auth-callback?origin=/dashboard");
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=/dashboard");
+
+  return <div>Dashboard</div>;
+}
