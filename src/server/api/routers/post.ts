9b0036f99b679ea5da767e0b1f2f7c6ed9ac9afe
TylerIllman <tyler.illman.productions@gmail.com>
2024-01-05 08:10:38 +0800

    update Prisma Schema

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,41 +2,10 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+export const authRouter = createTRPCRouter({
+  authCallback: publicProcedure.query(({ctx}) => {
+    const user = ctx.user
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
   }),
-
-  test: publicProcedure.query(({ctx}) => {
-    console.log("IN THE TEST ROUTE")
-
-    const user = ctx.user?.firstName
-    console.log("TRPC LOGGING USER: ",user)
-
-    return user
-  })
 });
 
