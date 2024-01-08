993fbfff98edec9cbee9f84ea54f3c80d0dbb70a
TylerIllman <tyler.illman.productions@gmail.com>
2024-01-08 14:43:13 +0800

    PDF render and tool bar complete

---
+++ src/app/components/PdfFullscreen.tsx
@@ -0,0 +1,73 @@
+import { Dialog } from "@radix-ui/react-dialog";
+import { Expand, Loader2 } from "lucide-react";
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
+import { useResizeDetector } from "react-resize-detector";
+import SimpleBar from "simplebar-react";
+import { Button } from "~/components/ui/button";
+import { DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { useToast } from "~/components/ui/use-toast";
+
+interface PdfFullscreenProps {
+  fileUrl: string;
+}
+
+const PdfFullscreen = ({ fileUrl }: PdfFullscreenProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [numPages, setNumPages] = useState<number | null>(null);
+
+  const { width, ref } = useResizeDetector();
+
+  const { toast } = useToast();
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button
+          onClick={() => setIsOpen(true)}
+          aria-label="fullscreen"
+          variant="ghost"
+          className="gap-1.5"
+        >
+          <Expand className="h-4 w-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-full max-w-7xl">
+        <SimpleBar autoHide={false} className="mt-6 max-h-[calc(100vh-10rem)]">
+          <div ref={ref}>
+            <Document
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={fileUrl}
+              className="max-h-full"
+            >
+              {new Array(numPages).fill(0).map((_, i) => (
+                <Page key={i} pageNumber={i + 1} width={width ? width : 1} />
+              ))}
+            </Document>
+          </div>
+        </SimpleBar>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PdfFullscreen;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import "simplebar-react/dist/simplebar.min.css";
+
 import Navbar from "./components/Navbar";
 import { cn } from "~/lib/utils";
 import { Toaster } from "~/components/ui/toaster";
