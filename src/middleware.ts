d76b3b90f21d69c09c5bf0c0924264875e2e60a8
TylerIllman <tyler.illman.productions@gmail.com>
2024-07-03 21:09:05 +0800

    added Middleware auth PROPER

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,32 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isTenantRoute = createRouteMatcher([
-  '/organization-selector(.*)',
-  '/orgid/(.*)'
-])
-
-const isTenantAdminRoute = createRouteMatcher([
-  '/orgId/(.*)/memberships',
-  '/orgId/(.*)/domain',
+  "/organization-selector(.*)",
+  "/orgid/(.*)",
 ]);
 
+const isTenantAdminRoute = createRouteMatcher([
+  "/orgId/(.*)/memberships",
+  "/orgId/(.*)/domain",
+]);
 
 export default clerkMiddleware((auth, req) => {
   // Restrict admin routes to users with specific permissions
   if (isTenantAdminRoute(req)) {
-    auth().protect(has => {
+    auth().protect((has) => {
       return (
-        has({ permission: 'org:sys_memberships:manage' }) ||
-        has({ permission: 'org:sys_domains_manage' })
-      )
-    })
+        has({ permission: "org:sys_memberships:manage" }) ||
+        has({ permission: "org:sys_domains_manage" })
+      );
+    });
   }
   // Restrict organization routes to signed in users
   if (isTenantRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-  '/dashboard(.*)',
-
-    export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
-  '/ferum(.*)',
-]);mport { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
@@ -47,7 +36,6 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 import { loggerLink } from "@trpc/client";
-import { createRouteMatcher } from "node_modules/@clerk/nextjs/dist/types/server/authMiddleware";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -68,4 +56,3 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
